a38779c9148a1ad867d785356c1f9627365a8fb0
Łukasz Golonka <lukasz.golonka@mailbox.org>
2023-12-17 17:31:52 +0100

    Add definitions of built-in translation functions

---
+++ typings/__builtins__.pyi
@@ -0,0 +1,14 @@
+def _(msg: str) -> str:
+	...
+
+
+def pgettext(context: str, message: str) -> str:
+	...
+
+
+def ngettext(msgid1: str, msgid2: str, n: int) -> str:
+	...
+
+
+def npgettext(context: str, msgid1: str, msgid2: str, n: int) -> str:
+	...
